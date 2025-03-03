8f406fed5e5048dd526f35f50e63b02e732a68e8
Brett Gamble <brettkgamble@gmail.com>
2025-03-03 14:57:14 -0700

    0009 - play button

--- ttm/src/app/components/AudioPlayer.js
+++ ttm/src/app/components/AudioPlayer.js
@@ -19,11 +19,11 @@ const AudioPlayer = () => {
   return (
     <div className={styles.audioPlayer}>
       <audio src="https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/602fd246-0a4f-49eb-83e5-41dc1f15ed50/audio/9fc78e12-f322-41d9-8a1b-05cfb40b3c69/default_tc.mp3?nocache" preload="metadata"></audio>
-      <button onClick={togglePlayPause}><BsArrowLeftShort/> 30</button>
-      <button>
+      <button className={styles.forwardBackward}><BsArrowLeftShort/> 30</button>
+      <button onClick={togglePlayPause} className={styles.playPause}>
         { isPlaying ? <FaPlay /> : <FaPause /> } 
       </button>
-      <button>30 <BsArrowRightShort /></button>
+      <button className={styles.forwardBackward}>30 <BsArrowRightShort /></button>
 
       {/* current time */}
       <div>0:00</div>
