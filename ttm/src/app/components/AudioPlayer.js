e959e351a56d16fa7c642beead88c4b22b9a2af2
Brett Gamble <brettkgamble@gmail.com>
2025-03-03 10:14:23 -0700

    0005 - AudioPlayer.  Managing state of the player.  Adding css module for audioPlayer

--- ttm/src/app/components/AudioPlayer.js
+++ ttm/src/app/components/AudioPlayer.js
@@ -1,15 +1,33 @@
-import React from 'react'
+'use client'
+import React from 'react';
+
+import { BsArrowLeftShort } from "react-icons/bs";
+import { BsArrowRightShort } from "react-icons/bs";
+import {FaPlay} from "react-icons/fa";
+import {FaPause} from "react-icons/fa";
+
 
 const AudioPlayer = () => {
+
+  const [isPlaying, setIsPlaying ] = React.useState();
+
   return (
     <div>
       <audio src="https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/602fd246-0a4f-49eb-83e5-41dc1f15ed50/audio/9fc78e12-f322-41d9-8a1b-05cfb40b3c69/default_tc.mp3?nocache" preload="metadata"></audio>
-      <button>back 30</button>
+      <button><BsArrowLeftShort/> 30</button>
       <button> play / pause</button>
-      <button>forward 30</button>
+      <button>30 <BsArrowRightShort /></button>
 
       {/* current time */}
       <div>0:00</div>
+
+      {/* progress bar */}
+      <div>
+        <input type="range"></input>
+      </div>
+
+      {/* duration */}
+      <div>2:49</div>
     </div>
   )
 }
--- ttm/src/app/page.tsx
+++ ttm/src/app/page.tsx
@@ -1,8 +1,9 @@
+import styles from './styles/AudioPlayer.module.css';
 import { AudioPlayer } from './components/AudioPlayer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen pt-0 px-6 justify-center items-center">
+    <div className={`${styles.audioPlayer} flex min-h-screen pt-0 px-6 justify-center items-center`}>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <AudioPlayer />
       </main>
