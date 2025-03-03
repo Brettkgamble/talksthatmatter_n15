4435024aa7f2743690b6d8e549907cd8ff6d7569
Brett Gamble <brettkgamble@gmail.com>
2025-03-03 16:10:33 -0700

    0010 - Progress bar styling

--- ttm/src/app/components/AudioPlayer.js
+++ ttm/src/app/components/AudioPlayer.js
@@ -21,20 +21,20 @@ const AudioPlayer = () => {
       <audio src="https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/602fd246-0a4f-49eb-83e5-41dc1f15ed50/audio/9fc78e12-f322-41d9-8a1b-05cfb40b3c69/default_tc.mp3?nocache" preload="metadata"></audio>
       <button className={styles.forwardBackward}><BsArrowLeftShort/> 30</button>
       <button onClick={togglePlayPause} className={styles.playPause}>
-        { isPlaying ? <FaPlay /> : <FaPause /> } 
+        { isPlaying ? <FaPlay className={styles.play}/> : <FaPause /> } 
       </button>
       <button className={styles.forwardBackward}>30 <BsArrowRightShort /></button>
 
       {/* current time */}
-      <div>0:00</div>
+      <div className={styles.currentTime}>0:00</div>
 
       {/* progress bar */}
       <div>
-        <input type="range"></input>
+        <input type="range" className={styles.progressBar}></input>
       </div>
 
       {/* duration */}
-      <div>2:49</div>
+      <div className={styles.duration}>2:49</div>
     </div>
   )
 }
