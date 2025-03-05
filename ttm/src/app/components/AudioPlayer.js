d8d21bb0661f7655808bbcfd4de00bf8aa8b0a5d
Brett Gamble <brettkgamble@gmail.com>
2025-03-04 17:25:28 -0700

    0012 - added useRef for AudioPlayer

--- ttm/src/app/components/AudioPlayer.js
+++ ttm/src/app/components/AudioPlayer.js
@@ -1,6 +1,6 @@
 'use client'
 import styles from '../styles/AudioPlayer.module.css';
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 import { BsArrowLeftShort } from "react-icons/bs";
 import { BsArrowRightShort } from "react-icons/bs";
@@ -9,19 +9,28 @@ import {FaPause} from "react-icons/fa";
 
 
 const AudioPlayer = () => {
-
+  // state
   const [isPlaying, setIsPlaying] = useState(false);
 
+  // references
+  const audioPlayer = useRef();  // referenc to our audio component
+
   const togglePlayPause = () => {
-    setIsPlaying(!isPlaying);
+    const prevValue = isPlaying;
+    setIsPlaying(!prevValue);
+    if (!prevValue) {
+      audioPlayer.current.play();
+    } else {
+      audioPlayer.current.pause();
+    }
   }
 
   return (
     <div className={styles.audioPlayer}>
-      <audio src="https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/602fd246-0a4f-49eb-83e5-41dc1f15ed50/audio/9fc78e12-f322-41d9-8a1b-05cfb40b3c69/default_tc.mp3?nocache" preload="metadata"></audio>
+      <audio ref={audioPlayer} src="https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/602fd246-0a4f-49eb-83e5-41dc1f15ed50/audio/9fc78e12-f322-41d9-8a1b-05cfb40b3c69/default_tc.mp3?nocache" preload="metadata"></audio>
       <button className={styles.forwardBackward}><BsArrowLeftShort/> 30</button>
       <button onClick={togglePlayPause} className={styles.playPause}>
-        { isPlaying ? <FaPlay className={styles.play}/> : <FaPause /> } 
+        { !isPlaying ? <FaPlay className={styles.play}/> : <FaPause /> } 
       </button>
       <button className={styles.forwardBackward}>30 <BsArrowRightShort /></button>
 
