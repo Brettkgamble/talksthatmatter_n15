9ba1cff35520397f94934333a82fbc5672568ee5
Brett Gamble <brettkgamble@gmail.com>
2025-03-05 17:27:51 -0700

    0014 - First vid complete

--- ttm/src/app/components/AudioPlayer.js
+++ ttm/src/app/components/AudioPlayer.js
@@ -12,14 +12,17 @@ const AudioPlayer = () => {
   // state
   const [isPlaying, setIsPlaying] = useState(false);
   const [duration, setDuration] = useState(0);
-  const [currentTime] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
 
   // references
-  const audioPlayer = useRef();  // referenc to our audio component
+  const audioPlayer = useRef();  // reference to our audio component
+  const progressBar = useRef();  // reference to progress bar
+  const animationRef = useRef(); // references the animation
 
   useEffect(()=> {
     const seconds = Math.floor(audioPlayer.current.duration);
     setDuration(seconds);
+    progressBar.current.max = seconds;
   }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
 
   const calculateTime = (secs) => {
@@ -35,26 +38,57 @@ const AudioPlayer = () => {
     setIsPlaying(!prevValue);
     if (!prevValue) {
       audioPlayer.current.play();
+      animationRef.current = requestAnimationFrame(whilePlaying);
     } else {
       audioPlayer.current.pause();
+      cancelAnimationFrame(animationRef.current);
     }
+  }
+
+  const whilePlaying =() => {
+    progressBar.current.value = audioPlayer.current.currentTime;
+    changePlayerCurrentTime();
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  }
+
+  const changeRange = () => {
+    audioPlayer.current.currentTime = progressBar.current.value;
+    changePlayerCurrentTime();
+  }
+
+  const changePlayerCurrentTime = () => {
+    progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`);
+    setCurrentTime(progressBar.current.value);
+  }
+
+  const backThirty = () => {
+    progressBar.current.value = Number(progressBar.current.value - 30);
+    changeRange();
+  }
+
+  const forwardThirty = () => {
+    progressBar.current.value = Number(progressBar.current.value + 30);
+    changeRange();
   }
 
   return (
     <div className={styles.audioPlayer}>
       <audio ref={audioPlayer} src="https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/602fd246-0a4f-49eb-83e5-41dc1f15ed50/audio/9fc78e12-f322-41d9-8a1b-05cfb40b3c69/default_tc.mp3?nocache" preload="metadata"></audio>
-      <button className={styles.forwardBackward}><BsArrowLeftShort/> 30</button>
+      
+      <button className={styles.forwardBackward} onClick={backThirty}><BsArrowLeftShort/> 30</button>
+      
       <button onClick={togglePlayPause} className={styles.playPause}>
         { !isPlaying ? <FaPlay className={styles.play}/> : <FaPause /> } 
       </button>
-      <button className={styles.forwardBackward}>30 <BsArrowRightShort /></button>
+      
+      <button className={styles.forwardBackward} onClick={forwardThirty}>30 <BsArrowRightShort /></button>
 
       {/* current time */}
       <div className={styles.currentTime}>{calculateTime(currentTime)}</div>
 
       {/* progress bar */}
       <div>
-        <input type="range" className={styles.progressBar} defaultValue="0"></input>
+        <input type="range" className={styles.progressBar} defaultValue="0" ref={progressBar} onChange={changeRange}></input>
       </div>
 
       {/* duration */}
