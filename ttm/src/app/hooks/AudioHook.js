8df0363ba765b83aa0ce0e3f930889b6e5b92c54
Brett Gamble <brettkgamble@gmail.com>
2025-03-13 10:50:17 -0600

    0021 - Hools completed

---
+++ ttm/src/app/hooks/AudioHook.js
@@ -0,0 +1,112 @@
+import { useState, useRef, useEffect } from "react";
+import { useSearchParams } from 'next/navigation';
+
+const useAudio = () => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [timeJump, setTimeJump] = useState(0);
+
+    const searchParams = useSearchParams();
+
+    // references
+    const audioPlayer = useRef();  // reference to our audio component
+    const progressBar = useRef();  // reference to progress bar
+    const animationRef = useRef(); // references the animation
+
+    // grabbing metadata
+    useEffect(()=> {
+        const seconds = Math.floor(audioPlayer.current.duration);
+        setDuration(seconds);
+        progressBar.current.max = seconds;
+    }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
+
+    //end of file
+    useEffect(()=> {
+        if (Number(duration) >= 0 && Number(currentTime) === Number(duration)) {
+            togglePlayPause();
+            timeTravel(0);
+        }
+    },[currentTime]);
+
+    useEffect(()=> {
+        if (timeJump && timeJump > 0) {
+            timeTravel(timeJump);
+            setIsPlaying(true);
+            play();
+        }
+    }, [timeJump])
+
+       
+    const calculateTime = (secs) => {
+        const minutes = Math.floor(secs / 60);
+        const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+        const seconds = Math.floor(secs % 60);
+        const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+        return `${returnedMinutes}:${returnedSeconds}`;
+    }
+
+    const play =() => {
+        audioPlayer.current.play();
+        animationRef.current = requestAnimationFrame(whilePlaying);
+    }
+
+    const togglePlayPause = () => {
+        const prevValue = isPlaying;
+        setIsPlaying(!prevValue);
+        if (!prevValue) {
+            play();
+        } else {
+            audioPlayer.current.pause();
+            cancelAnimationFrame(animationRef.current);
+        }
+    }
+
+    const whilePlaying =() => {
+        progressBar.current.value = audioPlayer.current.currentTime;
+        changePlayerCurrentTime();
+        animationRef.current = requestAnimationFrame(whilePlaying);
+    }
+    
+    const changeRange = () => {
+        audioPlayer.current.currentTime = progressBar.current.value;
+        changePlayerCurrentTime();
+    }
+
+    const changePlayerCurrentTime = () => {
+        progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`);
+        setCurrentTime(progressBar.current.value);
+    }
+
+    const backThirty = () => {
+        timeTravel(Number(progressBar.current.value) - 30);
+    }
+
+    const forwardThirty = () => {
+        timeTravel(Number(progressBar.current.value) + 30);
+    }
+
+    const timeTravel = (newTime) => {
+    progressBar.current.value = newTime;
+        changeRange();
+    }
+
+    return {
+        audioPlayer,
+        backThirty,
+        calculateTime,
+        changeRange,
+        currentTime,
+        duration,
+        forwardThirty,
+        isPlaying,
+        play,
+        progressBar,
+        setDuration,
+        setTimeJump,
+        timeTravel,
+        togglePlayPause,
+    }
+}
+
+export { useAudio };
--- ttm/src/app/page.tsx
+++ ttm/src/app/page.tsx
@@ -4,11 +4,25 @@ import { AudioPlayer } from './components/AudioPlayer';
 
 
 export default function Home() {
+    // pass in as a prop.  Params are seconds
+    const chapters = [
+      {
+        start: 0,
+        end: 15
+      },
+      {
+        start: 60,
+        end: 75,
+      },
+  ]
+
+  const track = "https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/602fd246-0a4f-49eb-83e5-41dc1f15ed50/audio/9fc78e12-f322-41d9-8a1b-05cfb40b3c69/default_tc.mp3?nocache"
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <Suspense>
-          <AudioPlayer />
+          <AudioPlayer chapters={chapters} track={track} />
         </Suspense>
       </main>
     </div>
